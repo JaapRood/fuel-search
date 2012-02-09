fd13fe55e16a93ff17d1cbb4225fec9bc4288893
Jaap Rood <jaap@wijzijnrood.nl>
2012-02-09 23:44:15 +0100

    Added an empty test, so we don't forget

---
+++ tests/search.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Search
+ *
+ * Search lets you search for stuff in data, allowing spelling mistakes!
+ *
+ * @package		Search
+ * @version		0.5
+ * @author		Jaap Rood / Ronald Mansveld
+ * @license		MIT License
+ * @link		http://github.com/JaapRood/fuel-search
+ */
+
+namespace Search;
+
+/**
+ * Search class tests
+ * 
+ * @group Search
+ */
+class Test_Search extends \Fuel\Core\TestCase {
+ 	public function test_foo() {}
+}
